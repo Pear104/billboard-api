6ab8fc05bffa54205be37a0d4f59f27da959f256
Truong <107944687+Pear104@users.noreply.github.com>
2024-09-07 20:45:23 +0700

    add week stat

--- src/index.ts
+++ src/index.ts
@@ -11,11 +11,45 @@ server.get("/hot-100", async (request, reply) => {
     .map((i, div) => {
       const title = $(div).find("h3#title-of-a-story").text().trim();
       const artist = $(div).find("span.c-label.a-no-trucate").text().trim();
-      const img = $(div).find("img.c-lazy-image__img").attr("src");
+      const statusNum = $(div)
+        .find("li:nth-child(3) g")
+        .attr("data-name")
+        ?.replace("Group ", "");
+      const status =
+        statusNum == "3"
+          ? "stayingSame"
+          : statusNum == "7170"
+          ? "movingUp"
+          : "fallingDown";
+      const img = $(div)
+        .find("img.c-lazy-image__img")
+        .attr("src")
+        ?.replace("180x180", "344x344");
+      const thisWeekPos = $(div)
+        .find("> li:nth-child(1) span:nth-child(1)")
+        .text()
+        .trim();
+      const lastWeekPos = $(div)
+        .find("li.lrv-u-width-100p:nth-child(4) > ul > li:nth-child(4) span")
+        .text()
+        .trim();
+      const peekPos = $(div)
+        .find("li.lrv-u-width-100p:nth-child(4) > ul > li:nth-child(5) span")
+        .text()
+        .trim();
+      const weeksOnChart = $(div)
+        .find("li.lrv-u-width-100p:nth-child(4) > ul > li:nth-child(6) span")
+        .text()
+        .trim();
       return {
         title,
         artist,
         img,
+        status: thisWeekPos === "-" ? "new" : status,
+        thisWeekPos,
+        lastWeekPos,
+        peekPos,
+        weeksOnChart,
       };
     })
     .get();
@@ -32,11 +66,35 @@ server.get("/billboard-200", async (request, reply) => {
     .map((i, div) => {
       const title = $(div).find("h3#title-of-a-story").text().trim();
       const artist = $(div).find("span.c-label.a-no-trucate").text().trim();
-      const img = $(div).find("img.c-lazy-image__img").attr("src");
+      const img = $(div)
+        .find("img.c-lazy-image__img")
+        .attr("src")
+        ?.replace("180x180", "344x344");
+      const thisWeekPos = $(div)
+        .find("> li:nth-child(1) span:nth-child(1)")
+        .text()
+        .trim();
+      const lastWeekPos = $(div)
+        .find("li.lrv-u-width-100p:nth-child(4) > ul > li:nth-child(4) span")
+        .text()
+        .trim();
+      const peekPos = $(div)
+        .find("li.lrv-u-width-100p:nth-child(4) > ul > li:nth-child(5) span")
+        .text()
+        .trim();
+      const weeksOnChart = $(div)
+        .find("li.lrv-u-width-100p:nth-child(4) > ul > li:nth-child(6) span")
+        .text()
+        .trim();
       return {
         title,
         artist,
         img,
+        isNew: thisWeekPos === "-" ? true : false,
+        thisWeekPos,
+        lastWeekPos,
+        peekPos,
+        weeksOnChart,
       };
     })
     .get();
@@ -53,31 +111,35 @@ server.get("/billboard-global-200", async (request, reply) => {
     .map((i, div) => {
       const title = $(div).find("h3#title-of-a-story").text().trim();
       const artist = $(div).find("span.c-label.a-no-trucate").text().trim();
-      const img = $(div).find("img.c-lazy-image__img").attr("src");
+      const img = $(div)
+        .find("img.c-lazy-image__img")
+        .attr("src")
+        ?.replace("180x180", "344x344");
+      const thisWeekPos = $(div)
+        .find("> li:nth-child(1) span:nth-child(1)")
+        .text()
+        .trim();
+      const lastWeekPos = $(div)
+        .find("li.lrv-u-width-100p:nth-child(4) > ul > li:nth-child(4) span")
+        .text()
+        .trim();
+      const peekPos = $(div)
+        .find("li.lrv-u-width-100p:nth-child(4) > ul > li:nth-child(5) span")
+        .text()
+        .trim();
+      const weeksOnChart = $(div)
+        .find("li.lrv-u-width-100p:nth-child(4) > ul > li:nth-child(6) span")
+        .text()
+        .trim();
       return {
         title,
         artist,
         img,
-      };
-    })
-    .get();
-
-  reply.send(data);
-});
-
-server.get("/artist-100", async (request, reply) => {
-  const html = await fetch("https://www.billboard.com/charts/artist-100").then(
-    (res) => res.text()
-  );
-  const $ = cheerio.load(html);
-  const data = $("div.chart-results-list ul.o-chart-results-list-row")
-    .map((i, div) => {
-      const artist = $(div).find("h3#title-of-a-story").text().trim();
-      // const artist = $(div).find("span.c-label.a-no-trucate").text().trim();
-      const img = $(div).find("img.c-lazy-image__img").attr("src");
-      return {
-        artist,
-        img,
+        isNew: thisWeekPos === "-" ? true : false,
+        thisWeekPos,
+        lastWeekPos,
+        peekPos,
+        weeksOnChart,
       };
     })
     .get();
@@ -94,9 +156,56 @@ server.get("/tiktok-billboard-top-50", async (request, reply) => {
     .map((i, div) => {
       const title = $(div).find("h3#title-of-a-story").text().trim();
       const artist = $(div).find("span.c-label.a-no-trucate").text().trim();
-      const img = $(div).find("img.c-lazy-image__img").attr("src");
+      const img = $(div)
+        .find("img.c-lazy-image__img")
+        .attr("src")
+        ?.replace("180x180", "344x344");
+      const thisWeekPos = $(div)
+        .find("> li:nth-child(1) span:nth-child(1)")
+        .text()
+        .trim();
+      const lastWeekPos = $(div)
+        .find("li.lrv-u-width-100p:nth-child(4) > ul > li:nth-child(3) span")
+        .text()
+        .trim();
+      const peekPos = $(div)
+        .find("li.lrv-u-width-100p:nth-child(4) > ul > li:nth-child(4) span")
+        .text()
+        .trim();
+      const weeksOnChart = $(div)
+        .find("li.lrv-u-width-100p:nth-child(4) > ul > li:nth-child(5) span")
+        .text()
+        .trim();
       return {
         title,
+        artist,
+        img,
+        isNew: thisWeekPos === "-" ? true : false,
+        thisWeekPos,
+        lastWeekPos,
+        peekPos,
+        weeksOnChart,
+      };
+    })
+    .get();
+
+  reply.send(data);
+});
+
+server.get("/artist-100", async (request, reply) => {
+  const html = await fetch("https://www.billboard.com/charts/artist-100").then(
+    (res) => res.text()
+  );
+  const $ = cheerio.load(html);
+  const data = $("div.chart-results-list ul.o-chart-results-list-row")
+    .map((i, div) => {
+      const artist = $(div).find("h3#title-of-a-story").text().trim();
+      // const artist = $(div).find("span.c-label.a-no-trucate").text().trim();
+      const img = $(div)
+        .find("img.c-lazy-image__img")
+        .attr("src")
+        ?.replace("180x180", "344x344");
+      return {
         artist,
         img,
       };
@@ -115,3 +224,6 @@ server.listen({ port: 3000 }, function (err, address) {
 
   console.log(`Server is now listening on ${address}`);
 });
+
+// https://charts-static.billboard.com/img/2024/09/travis-scott-lu8-daysbeforerodeo-5ue-344x344.jpg
+// https://charts-static.billboard.com/img/2024/09/travis-scott-lu8-daysbeforerodeo-5ue-180x180.jpg
